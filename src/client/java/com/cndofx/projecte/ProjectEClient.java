b4e16c501eea8e8d66f76accd318ce9ae7143f07
cndofx <19335833+cndofx@users.noreply.github.com>
2023-10-23 20:31:21 -0600

    project template

---
+++ src/client/java/com/cndofx/projecte/ProjectEClient.java
@@ -0,0 +1,10 @@
+package com.cndofx.projecte;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectEClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/cndofx/projecte/ProjectEDataGenerator.java
@@ -0,0 +1,11 @@
+package com.cndofx.projecte;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ProjectEDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
