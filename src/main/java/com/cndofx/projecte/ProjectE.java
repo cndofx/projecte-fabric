0c77a2b7179fff9bdc512eb12a4d51cb28ad9338
cndofx <19335833+cndofx@users.noreply.github.com>
2023-10-25 00:48:01 -0600

    move item group definition to its own file

--- src/main/java/com/cndofx/projecte/ProjectE.java
+++ src/main/java/com/cndofx/projecte/ProjectE.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.cndofx.projecte.block.ModBlocks;
+import com.cndofx.projecte.item.ModItemGroups;
 import com.cndofx.projecte.item.ModItems;
 
 public class ProjectE implements ModInitializer {
@@ -15,7 +16,8 @@ public class ProjectE implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
-		ModItems.registerItems();
 		ModBlocks.registerBlocks();
+		ModItems.registerItems();
+		ModItemGroups.registerItemGroups();
 	}
 }
--- src/main/java/com/cndofx/projecte/item/ModItems.java
+++ src/main/java/com/cndofx/projecte/item/ModItems.java
@@ -1,17 +1,11 @@
 package com.cndofx.projecte.item;
 
 import com.cndofx.projecte.ProjectE;
-import com.cndofx.projecte.block.ModBlocks;
-
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
@@ -21,23 +15,6 @@ public class ModItems {
     public static final Item ALCHEMICAL_COAL = registerItem("alchemical_coal", new Item(new FabricItemSettings()));
     public static final Item MOBIUS_FUEL = registerItem("mobius_fuel", new Item(new FabricItemSettings()));
     public static final Item AETERNALIS_FUEL = registerItem("aeternalis_fuel", new Item(new FabricItemSettings()));
-
-    public static final ItemGroup PROJECTE_GROUP = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(ProjectE.MOD_ID, "projectegroup"),
-            FabricItemGroup
-                    .builder()
-                    .displayName(Text.translatable("projectegroup"))
-                    .icon(() -> new ItemStack(ModItems.PHILOSOPHERS_STONE))
-                    .entries((displayContext, entries) -> {
-                        entries.add(PHILOSOPHERS_STONE);
-                        entries.add(ALCHEMICAL_COAL);
-                        entries.add(MOBIUS_FUEL);
-                        entries.add(AETERNALIS_FUEL);
-
-                        entries.add(ModBlocks.ALCHEMICAL_COAL_BLOCK);
-                        entries.add(ModBlocks.MOBIUS_FUEL_BLOCK);
-                        entries.add(ModBlocks.AETERNALIS_FUEL_BLOCK);
-                    }).build());
 
     public static void registerItems() {
         ProjectE.LOGGER.info("Registering mod items");
